b70ea8256250a28f2f3ed9c5bc36da524c1c3758
Romain "dreamstate" Pichot <ropichot@gmail.com>
2017-05-18 00:09:54 +0200

    Get post data

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const
   express           = require('express'),
+  bodyParser       = require('body-parser'),
   config            = require('./config'),
   RestaurantFinder  = require('./RestaurantFinder')
 ;
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.post('/eatnow', (req, res) => {
-  console.log(req.query);
+  console.log(req.body);
+  res.json({'success' => 'true'});
 });
 
 app.listen(config.port, config.host, (err) => {
